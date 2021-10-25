9061c54e5bf24170ad2a9ce8d4836e6328b7fc26
oveigam <oveigam@gmail.com>
2021-10-25 23:01:51 +0200

    refresh route

--- src/routes/auth-router.js
+++ src/routes/auth-router.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
-const { login } = require('../controllers/auth-controller')
+const { login, refresh } = require('../controllers/auth-controller')
 
 const authRouter = Router()
 
 authRouter.post('/login', login)
+authRouter.post('/refresh', refresh)
 
 module.exports = authRouter
